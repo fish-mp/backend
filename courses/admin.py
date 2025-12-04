2d0e038f4395625ff62de47dac05f83430119551
alt1tla <GerdzhikovaVeronWench@gmail.com>
2025-12-04 11:44:02 +0300

    upd course's tag

--- courses/admin.py
+++ courses/admin.py
@@ -1,13 +1,33 @@
-from unfold.admin import ModelAdmin, TabularInline
+from unfold.admin import ModelAdmin, StackedInline
 from django.contrib import admin
+from courses.models import *
 
 # Register your models here.
+class CourseInline(StackedInline):
+    model = Course
+    extra = 0  # Не показывать пустые формы для новых курсов
+    fields = ['title', 'short_description']
+    show_change_link = True  # Показывать ссылку на редактирование курса
 
-from courses.models import Course, Files, Enrollment
+@admin.register(Tag)
+class TagAdmin(ModelAdmin):
+    list_display = ('name', 'course_count')
+    search_fields = ('name',)
+    inlines = [CourseInline]  # Показывать курсы с этим тегом
+    
+    def course_count(self, obj):
+        return obj.course_set.count()
+    course_count.short_description = 'Количество курсов'
+
 @admin.register(Course)
 class CourseAdmin(ModelAdmin):
-    list_display = ('title', 'description', 'short_description')
+    list_display = ('title', 'short_description', 'get_tag')
     search_fields = ('title', 'description')
+    
+    def get_tag(self, obj):
+        return obj.tag.name if obj.tag else 'Нет тега'
+    get_tag.short_description = 'Тег'
+
 @admin.register(Files)
 class FilesAdmin(ModelAdmin):
     list_display = ('title', 'file')
