542330c0c4e7d5e4642bdc5da39684a4655a4b2c
Berlkot <91618103+Berlkot@users.noreply.github.com>
2026-05-06 14:43:10 +0300

    move swagger to be accessible in deploy build

--- service/urls.py
+++ service/urls.py
@@ -39,7 +39,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -50,4 +49,5 @@ urlpatterns = [
     path('api/news/', include('news.urls')),
     path('api/courses/', include('courses.urls')),
     path('api/', include('shop.urls')),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
