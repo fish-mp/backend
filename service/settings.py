311336c47f0130b85b1f4842e6f642de7475421c
ndbeliaeb0401 <cguggffvb@gmail.com>
2026-05-12 16:00:04 +0300

    fix

--- service/settings.py
+++ service/settings.py
@@ -138,7 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1488),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
